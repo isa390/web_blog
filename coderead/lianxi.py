6619fca0569e01619ed3a68b9a10c9d435c22a47
isa <925795310@qq.com>
2022-03-15 01:24:15 +0800

    自动化学习心得及自动化框架用例补充心得

--- coderead/lianxi.py
+++ coderead/lianxi.py
@@ -1,2 +1,11 @@
-if __name__=="__main__":   #以下代码只在本模块运行
-    print(__name__)
+# if __name__=="__main__":   #以下代码只在本模块运行
+#     print(__name__)
+#     class changfangxing
+#       def __init__(self,chang,kuan) :
+#           self.chang = chang
+#           self.kuan = kuan
+#       def zhouchang(self):
+#           return(self.chang+self.kuan)*2
+         
+print(hasattr(str,"replacre"))
+    
