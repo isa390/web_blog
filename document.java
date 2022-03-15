464f7567eaeaf492502397a048132e2b90b14182
NewMike <2454069901@qq.com>
2022-03-15 21:25:56 +0800

    iniit

--- document.java
+++ document.java
@@ -1,3 +1,58 @@
+getNextLine('blank01')+''+
+getNextLine('blank00')+'@Slf4j'+
+getNextLine('blank00')+'public class OpenDocumentDocCase extends BaseOpenPlatformCase {'+
+getNextLine('blank04')+'    @BeforeClass'+
+getNextLine('blank04')+'    public void beforeClass(){'+
+getNextLine('blank08')+'        SM.downloadFile.downloadFile(new Params()'+
+getNextLine('blank16')+'                .addParams("url", "http://tosv.boe.byted.org/obj/lark-qa-yinxiao/testFile/testDoc.doc").build());'+
+getNextLine('blank04')+'    }'+
+getNextLine('blank01')+''+
+getNextLine('blank04')+'    @Override'+
+getNextLine('blank04')+'    public Object[][] params() {'+
+getNextLine('blank08')+'        return new Object[][]{'+
+getNextLine('blank16')+'                new BaseDataBean()'+
+getNextLine('blank24')+'                        .setParam(new Params()'+
+getNextLine('blank32')+'                                .addParams("filePath", CM.downloadFileContext.tempFilePath)'+
+getNextLine('blank32')+'                                .addParams("fileType", "doc")'+
+getNextLine('blank32')+'                                .addParams("showMenu", true)'+
+getNextLine('blank32')+'                                .build())'+
+getNextLine('blank24')+'                        .setExpect(buildStatusExpect(SM.openDocument.apiName(),"ok"))'+
+getNextLine('blank24')+'                        .setAssertType(AssertType.STRING)'+
+getNextLine('blank24')+'                        .setDescription("desc")'+
+getNextLine('blank25')+'                         .build(),'+
+getNextLine('blank08')+'        };'+
+getNextLine('blank04')+'    }'+
+getNextLine('blank01')+''+
+getNextLine('blank04')+'    @Override'+
+getNextLine('blank04')+'    public boolean isAsync() {'+
+getNextLine('blank08')+'        return true;'+
+getNextLine('blank04')+'    }'+
+getNextLine('blank01')+''+
+getNextLine('blank04')+'    @Override'+
+getNextLine('blank04')+'    public OpenPlatformBaseStep currentStep() {'+
+getNextLine('blank08')+'        return SM.openDocument;'+
+getNextLine('blank04')+'    }'+
+getNextLine('blank01')+''+
+getNextLine('blank04')+'    @Override'+
+getNextLine('blank04')+'    public void doAsyncStep(Map<String, Object> params,NetCallback callback) {'+
+getNextLine('blank08')+'        SM.openDocument.openDocument(params, callback);'+
+getNextLine('blank01')+''+
+getNextLine('blank04')+'    }'+
+getNextLine('blank01')+''+
+getNextLine('blank04')+'    @Override'+
+getNextLine('blank04')+'    public void doAfterStepNow(Map<String, Object> otherParam, Map<String, Object> params) {'+
+getNextLine('blank08')+'        DM.D1().openDocumentPage().clickBack();'+
+getNextLine('blank04')+'    }'+
+getNextLine('blank01')+''+
+getNextLine('blank04')+'    @Override'+
+getNextLine('blank04')+'    public void doBeforeStep(Map<String, Object> otherParam, Map<String, Object> params) {'+
+getNextLine('blank04')+'    }'+
+getNextLine('blank00')+'}'+
+'+
+getNextLine('blank00')+'}'+
+
+
+
 
 @Slf4j
 public class OpenDocumentDocCase extends BaseOpenPlatformCase {
