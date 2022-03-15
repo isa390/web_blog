514967de5086fb7c6497c3038ddb01c578e89d13
NewMike <2454069901@qq.com>
2022-03-15 16:11:20 +0800

    init

---
+++ viewme/viewTheWorld/js/coder.js
@@ -0,0 +1,14 @@
+function keydown(topic){
+    if(event.keyCode == 13){ 
+        uploaddata(topic);
+     }
+}
+
+function uploaddata(topic){
+    console.log("ok")
+    var data = document.getElementById(topic).value;
+    var DM = 
+    var CM = 
+    var 
+    document.querySelector('.info').innerHTML = r; //显示
+}
