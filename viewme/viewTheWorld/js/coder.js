cf408665cb74267dde7445381cc6f4b5029319e9
isa <925795310@qq.com>
2022-03-15 16:28:44 +0800

    init

--- viewme/viewTheWorld/js/coder.js
+++ viewme/viewTheWorld/js/coder.js
@@ -7,8 +7,15 @@ function keydown(topic){
 function uploaddata(topic){
     console.log("ok")
     var data = document.getElementById(topic).value;
-    var DM = 
-    var CM = 
-    var 
-    document.querySelector('.info').innerHTML = r; //显示
+    var steps = "1,在business/openplatform/api/logic里面创建一个文件，如果有UI操作，可以借鉴OpenDocument.java<br>"+
+    "";
+    var DM = 1;
+    var CM = 2;
+    var SM = 1;
+    var CASE = 1;
+    var PAGE = 1;
+
+    var OUTPUT = steps+DM+CM+SM+CASE+PAGE;
+
+    document.querySelector('.info').innerHTML = OUTPUT; //显示
 }
