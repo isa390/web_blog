398627a8faacb29c46ac3057c2e427059acb8b3f
NewMike <2454069901@qq.com>
2022-03-15 16:42:55 +0800

    init

--- viewme/viewTheWorld/js/coder.js
+++ viewme/viewTheWorld/js/coder.js
@@ -7,7 +7,7 @@ function keydown(topic){
 function uploaddata(topic){
     console.log("ok")
     var data = document.getElementById(topic).value;
-    var steps = "1,在business/openplatform/api/logic里面创建一个文件，如果有UI操作，可以借鉴OpenDocument.java<br>"+
+    var steps = "操作步骤：<br>1,在business/openplatform/api/logic里面创建一个文件，如果有UI操作，可以借鉴OpenDocument.java<br>"+
     "";
     var DM = 1;
     var CM = 2;
@@ -16,6 +16,15 @@ function uploaddata(topic){
     var PAGE = 1;
 
     var OUTPUT = steps+DM+CM+SM+CASE+PAGE;
-
+    firstLetterToUpcase(data);
     document.querySelector('.info').innerHTML = OUTPUT; //显示
+}
+
+function firstLetterToUpcase(input){
+   if(input.length > 2){
+      var fisrt = input.substring( 0 ,1 ).toUpperCase();
+      var last = input.substring(1);
+      var output = fisrt+last;
+      console.log(output);
+   }
 }
